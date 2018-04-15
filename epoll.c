12bd9ed3f594de0ab63da86340b612cb724a6141
Barry Song <21cnbao@gmail.com>
2018-04-15 11:20:22 +0800

    enable pipe and globalfifo for epoll example
    Signed-off-by: Barry Song <21cnbao@gmail.com>

--- epoll.c
+++ epoll.c
@@ -1,54 +1,51 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <strings.h>
-#include <time.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/epoll.h>
+#include <sys/stat.h>
 
 static void call_epoll(void)
 {
-	int epfd;
-	struct epoll_event ev_stdin;
-	int err;
+	int epfd, fifofd, pipefd;
+	struct epoll_event ev, events[2];
+	int ret;
 
-	epfd = epoll_create(1);
+	epfd = epoll_create(2);
 	if (epfd < 0) {
 		perror("epoll_create()");
 		return;
 	}
 
-	bzero(&ev_stdin, sizeof(struct epoll_event));
-	ev_stdin.events =
-	    //  available for read operations
-	    EPOLLIN | EPOLLPRI
-	    // available for write operations
-	    // | EPOLLOUT
-	    // Error condition && Hang up happened
-	    | EPOLLERR | EPOLLHUP
-	    // Sets the Edge Triggered behaviour
-	    | EPOLLET
-	    // Sets the one-shot behaviour.
-	    // must call epoll_ctl with EPOLL_CTL_MOD to re-enable
-	    | EPOLLONESHOT;
+	ev.events = EPOLLIN|EPOLLET;
 
-	err = epoll_ctl(epfd, EPOLL_CTL_ADD, 1, &ev_stdin);
-	if (err < 0) {
-		perror("epoll_ctl()");
-		goto _out;
+	fifofd = open("/dev/globalfifo", O_RDONLY, S_IRUSR);
+	printf("fifo fd:%d\n", fifofd);
+	ev.data.fd = fifofd;
+	ret = epoll_ctl(epfd, EPOLL_CTL_ADD, fifofd, &ev);
+
+	pipefd = open("pipe", O_RDONLY|O_NONBLOCK, S_IRUSR);
+	printf("pipe fd:%d\n", pipefd);
+	ev.data.fd = pipefd;
+	ret = epoll_ctl(epfd, EPOLL_CTL_ADD, pipefd, &ev);
+
+	while(1) {
+		ret = epoll_wait(epfd, events, 2, 50000);
+		if (ret < 0) {
+			perror("epoll_wait()");
+		} else if (ret == 0) {
+			printf("No data within 50 seconds.\n");
+		} else {
+			int i;
+			for(i=0;i<ret;i++) {
+				char buf[100];
+				read(events[i].data.fd, buf, 100);
+				printf("%s is available now:, %s\n",
+						events[i].data.fd==fifofd? "fifo":"pipe", buf);
+			}
+		}
 	}
-	err = epoll_wait(epfd, &ev_stdin, 1, 5000);
-	if (err < 0) {
-		perror("epoll_wait()");
-	} else if (err == 0) {
-		printf("No data within five seconds.\n");
-	} else {
-		printf("Data is available now.\n");
-	}
-	err = epoll_ctl(epfd, EPOLL_CTL_DEL, 1, &ev_stdin);
-	if (err < 0) {
-		perror("epoll_ctl()");
-	}
- _out:
+_out:
 	close(epfd);
 }
 
