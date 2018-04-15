0f4919ae05608215c76748f9fc1760238d294e62
Barry Song <21cnbao@gmail.com>
2018-04-15 13:26:25 +0800

    add globalfifo cmd for event example
    Signed-off-by: Barry Song <21cnbao@gmail.com>

--- io-models/event-exam.c
+++ io-models/event-exam.c
@@ -4,9 +4,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <event.h>
 
-static void cmd_event(int fd, short events, void *arg)
+int fifofd;
+
+static void fifo_event(int fd, short events, void *arg)
 {
 	char msg[1024];
 
@@ -15,23 +18,40 @@ static void cmd_event(int fd, short events, void *arg)
 		perror("read fail ");
 		exit(1);
 	}
-	msg[ret]='\0';
-	printf("%s", msg);
+	printf("fifo:%s\n", msg);
+}
+
+static void std_event(int fd, short events, void *arg)
+{
+	char msg[1024];
+
+	int ret = read(fd, msg, sizeof(msg));
+	if (ret <= 0) {
+		perror("read fail ");
+		exit(1);
+	}
+	printf("stdin:%s\n", msg);
 }
 
 int main(int argc, char **argv)
 {
-	struct event ev_cmd;
-	
+	struct event std, fifo;
+	int fifofd;
+	fifofd = open("/dev/globalfifo", O_RDONLY, S_IRUSR);
+	printf("fifo fd:%d\n", fifofd);
+
 	event_init();
 	
-	event_set(&ev_cmd, STDIN_FILENO,
-		  EV_READ | EV_PERSIST, cmd_event, NULL);
-	event_add(&ev_cmd, NULL);
+	event_set(&std, STDIN_FILENO,
+		  EV_READ | EV_PERSIST, std_event, NULL);
+	event_set(&fifo, fifofd,
+		EV_READ | EV_PERSIST, fifo_event, NULL);
+
+	event_add(&std, NULL);
+	event_add(&fifo, NULL);
 
 	event_dispatch();
 
 	return 0;
 }                                                                                  
-
 
